d8ce29c420f16ec26c8a7dd7f408de25e8b56355
MorisPae <kiyofumi.mori138@gmail.com>
2017-06-13 10:01:29 +0900

    usersControllerにlogin, logout関数を追加

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -6,11 +6,34 @@ use App\Controller\AppController;
 use Cake\Event\Event;
 
 class UsersController extends AppController {
+
+    //その他のメソッド..
+
     //コントローラの各アクションの前に実行されます。
     //アクティブセッションのチェックや、ユーザー権限の検査をするために役立ちます。
     public function beforeFilter(Event $event) {
         parent::beforeFilter($event);
-        $this->Auth->allow('add');
+        //Allow users to register and logout.
+        //You should not add the "login" action to allow list. Doing so would
+        //cause problems with mormal functioning of AuthComponet.
+        $this->Auth->allow('add', 'logout');
+    }
+
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error(__('Invalid username or password, try again'));
+        }
+    }
+
+    public function logout()
+    {
+        return $this->redirect($this->Auth->logout());
     }
 
     public function index() {

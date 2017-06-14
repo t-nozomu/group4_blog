09425dfb51a0d01adfa56e5829dd91d2cf9e083c
MorisPae <kiyofumi.mori138@gmail.com>
2017-06-14 14:03:47 +0900

    カテゴリコンポーネントの削除

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -44,8 +44,8 @@
 
             // Just added the categories list to be able to choose
             // one category for an article
-            $categories = $this->Articles->Categories->find('treeList');
-            $this->set(compact('categories'));
+            //$categories = $this->Articles->Categories->find('treeList');
+            //$this->set(compact('categories'));
         }
 
         public function edit($id = null) {
